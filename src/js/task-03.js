af05d40c9a1573956cce097304b27b52f32474d1
Dmitriy Goncharov <my.webpage057@gmail.com>
2022-07-03 02:00:05 +0300

    add third task

--- src/js/task-03.js
+++ src/js/task-03.js
@@ -1,14 +1,43 @@
+// Напиши скрипт для создания галереи изображений по массиву данных. В HTML есть список ul.gallery.
+// Используй массив объектов images для создания элементов <img> вложенных в <li>. Для создания разметки используй шаблонные строки и метод insertAdjacentHTML().
+
+// Все элементы галереи должны добавляться в DOM за одну операцию вставки.
+// Добавь минимальное оформление галереи флексбоксами или гридами через CSS классы.
+
 const images = [
   {
-    url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'White and Black Long Fur Cat',
+    url: "https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "White and Black Long Fur Cat",
   },
   {
-    url: 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
+    url: "https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "Orange and White Koi Fish Near Yellow Koi Fish",
   },
   {
-    url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'Group of Horses Running',
+    url: "https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "Group of Horses Running",
   },
 ];
+
+// 1- получить доступ к ul
+// 2 - перебрать масив images
+// 3 - создать li
+// 4 - в li coздать тег img через метод insertAdjacentHTML()
+// 5 -  в img добавить каждый image
+
+const galleryEl = document.querySelector(".gallery");
+const imgGallery = images
+  .map(({ url, alt }) => {
+    console.log(alt);
+
+    return `
+    <li>
+      <img
+        alt='${alt}'
+        src='${url}'
+      >
+     </li>`;
+  })
+  .join(" ");
+
+galleryEl.insertAdjacentHTML("beforeend", `${imgGallery}`);
