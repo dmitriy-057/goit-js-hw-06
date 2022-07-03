83b85fb3618dd31fd38f4a9a52758c946eff9df0
Dmitriy Goncharov <my.webpage057@gmail.com>
2022-07-03 23:51:01 +0300

    add fourth task

---
+++ src/js/task-04.js
@@ -0,0 +1,24 @@
+// Счетчик состоит из спана и кнопок, которые, при клике, должны увеличивать и уменьшать его значение на единицу.
+// Создай переменную counterValue в которой будет храниться текущее значение счетчика и инициализируй её значением 0.
+// Добавь слушатели кликов на кнопки, внутри которых увеличивай или уменьшай значение счтетчика.
+// Обновляй интерфейс новым значением переменной counterValue.
+
+// 1- получить доступ к елементам value, decrement, increment
+// 2 - добавить слушателя на decrement, increment
+// 3 - при клике на decrement значение value -= 1
+// 4 -  при клике на increment значение value += 1
+
+const valueEl = document.querySelector("#value");
+console.log(valueEl);
+const decrementEl = document.querySelector('[data-action="decrement"]');
+console.log(decrementEl);
+const incrementEl = document.querySelector('[data-action="increment"]');
+console.log(incrementEl);
+
+decrementEl.addEventListener("click", () => {
+  valueEl.textContent -= 1;
+});
+
+incrementEl.addEventListener("click", () => {
+  valueEl.textContent += 1;
+});
