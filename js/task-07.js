654446cfccd161069c52f8139aba7ffd08e056f4
Dmitriy Goncharov <my.webpage057@gmail.com>
2022-07-06 00:04:55 +0300

    add seventh task after check

--- js/task-07.js
+++ js/task-07.js
@@ -4,6 +4,7 @@
 const inputEl = document.querySelector("#font-size-control");
 const textEl = document.querySelector("#text");
 
-inputEl.addEventListener("input", () => {
-  textEl.style.fontSize = `${inputEl.value}px`;
+inputEl.addEventListener("input", (event) => {
+  textEl.style.fontSize = event.currentTarget.value + "px";
+  console.log(textEl.style.fontSize);
 });
